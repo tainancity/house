1b438b949d24d392f55d891a2095a24e4d5ecb38
kiang <kiange@gmail.com>
2016-04-14 16:53:34 +0800

    basic search for lands

---
+++ Controller/LandsController.php
@@ -0,0 +1,78 @@
+<?php
+
+App::uses('AppController', 'Controller');
+
+class LandsController extends AppController {
+
+    public $name = 'Lands';
+    public $paginate = array();
+    public $helpers = array();
+
+    public function beforeFilter() {
+        parent::beforeFilter();
+        if (isset($this->Auth)) {
+            $this->Auth->allow(array('q'));
+        }
+    }
+
+    public function q($address = '') {
+        $this->autoRender = false;
+        $this->response->type('json');
+        $result = array(
+            'queryString' => $address,
+            'result' => array(),
+        );
+        $address = preg_replace('/\s+/', '', $address);
+        $pos = strrpos($address, '段');
+        if (false !== $pos) {
+            $sectionPart = substr($address, 0, $pos);
+            if (!empty($sectionPart)) {
+                $sections = $this->Land->Section->find('all', array(
+                    'conditions' => array('Section.name LIKE' => '%' . $sectionPart . '%'),
+                    'limit' => 10,
+                ));
+                $landPart = substr($address, $pos);
+                $numbersMap = array('０' => 0, '１' => 1, '２' => 2, '３' => 3, '４' => 4, '５' => 5, '６' => 6, '７' => 7, '８' => 8, '９' => 9,);
+                $landPart = strtr($landPart, $numbersMap);
+                $landPart = preg_replace('/[^0-9]/', '', $landPart);
+                if (count($sections) === 1 && !empty($landPart)) {
+                    $landPartLength = strlen($landPart);
+                    if ($landPartLength <= 4) {
+                        $landPart = str_pad($landPart, 4, '0', STR_PAD_LEFT) . '%';
+                    } elseif ($landPartLength < 8) {
+                        $landPart .= '%';
+                    } elseif ($landPartLength > 8) {
+                        $landPart = substr($landPart, 0, 8);
+                    }
+                    $conditions = array(
+                        'Land.section_id' => $sections[0]['Section']['id'],
+                    );
+                    if (false !== strpos($landPart, '%')) {
+                        $conditions['Land.code LIKE'] = $landPart;
+                    } else {
+                        $conditions['Land.code'] = $landPart;
+                    }
+                    $lands = $this->Land->find('all', array(
+                        'conditions' => $conditions,
+                        'limit' => 10,
+                    ));
+                    foreach ($lands AS $k => $item) {
+                        $item['Land']['label'] = $item['Land']['value'] = "{$sections[0]['Section']['name']}{$item['Land']['code']}";
+                        $result['result'][] = $item['Land'];
+                    }
+                } else {
+                    foreach ($sections AS $k => $item) {
+                        $item['Section']['label'] = $item['Section']['value'] = $item['Section']['name'];
+                        $result['result'][] = $item['Section'];
+                    }
+                }
+            }
+        }
+        if (!isset($_GET['pretty'])) {
+            $this->response->body(json_encode($result));
+        } else {
+            $this->response->body(json_encode($result, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+        }
+    }
+
+}
