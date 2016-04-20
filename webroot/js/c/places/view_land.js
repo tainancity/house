8cfceeb1c4353105769bb472e3159026c57dfcd3
kiang <kiange@gmail.com>
2016-04-20 15:46:42 +0800

    fix land place viewing

--- webroot/js/c/places/view_land.js
+++ webroot/js/c/places/view_land.js
@@ -1,46 +1,44 @@
 var map, marker;
 var loadedJson = {}, loadingFile = '';
 $(function () {
+    map = new google.maps.Map(document.getElementById('mapCanvas'), {
+        zoom: 18,
+        scaleControl: true,
+        navigationControl: true,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
     if (place.Land) {
-        map = new google.maps.Map(document.getElementById('mapCanvas'), {
-            zoom: 18,
-            scaleControl: true,
-            navigationControl: true,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        });
         $.getJSON(jsonBaseUrl + place.Land.file, {}, function (r) {
             for (k in r.features) {
                 if (place.Land.code == r.features[k].properties.AA49) {
+                    console.log(place);
+                    console.log(r.features[k].properties);
                     var newBounds = new google.maps.LatLngBounds;
-                    for (k in obj.geometry.coordinates) {
-                        for (j in obj.geometry.coordinates[k]) {
-                            newBounds.extend(new google.maps.LatLng(obj.geometry.coordinates[k][j][1], obj.geometry.coordinates[k][j][0]));
+                    for (k in r.features[k].geometry.coordinates) {
+                        for (j in r.features[k].geometry.coordinates[k]) {
+                            newBounds.extend(new google.maps.LatLng(r.features[k].geometry.coordinates[k][j][1], r.features[k].geometry.coordinates[k][j][0]));
                         }
                     }
                     map.fitBounds(newBounds);
                     if (!pointLatLng) {
-                        var centerPoint = newBounds.getCenter();
-                        marker = new google.maps.Marker({
-                            position: centerPoint,
-                            map: map,
-                            title: '土地'
-                        });
+                        pointLatLng = newBounds.getCenter();
                     }
+                    marker = new google.maps.Marker({
+                        position: pointLatLng,
+                        map: map,
+                        title: '土地'
+                    });
+                    map.setCenter(pointLatLng);
+                    map.data.addGeoJson(r.features[k]);
                 }
             }
         });
     } else if (pointLatLng) {
-        map = new google.maps.Map(document.getElementById('mapCanvas'), {
-            zoom: 18,
-            center: pointLatLng,
-            scaleControl: true,
-            navigationControl: true,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        });
         marker = new google.maps.Marker({
             position: pointLatLng,
             map: map,
             title: '土地'
         });
+        map.setCenter(pointLatLng);
     }
 });
